994e0822e12081930444d99bcf0d142f3eea56d7
Vikas Pritam <87309275+pritam-ago@users.noreply.github.com>
2025-02-09 22:36:22 +0530

    final draft

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://donation-platform-api.vercel.app',  
+  baseURL: 'http://localhost:5555',  
   headers: {
     'Authorization': `Bearer ${localStorage.getItem('token')}`,
   },
